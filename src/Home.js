c4bfff59565693b92d6c4936f7f8cfc2b21564e2
Sushant Prasad <sushantprasad1234@gmail.com>
2022-09-09 19:01:07 +0530

    committed

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+const Home = () => {
+  const { openSideBar, openModal } = useGlobalContext();
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSideBar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show Modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/context.js
@@ -0,0 +1,36 @@
+import React, { useState, useContext } from "react";
+const AppContext = React.createContext();
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openSideBar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSideBar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        openSideBar,
+        closeModal,
+        closeSideBar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+export { AppContext, AppProvider };
